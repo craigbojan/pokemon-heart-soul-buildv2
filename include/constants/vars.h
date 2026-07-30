589ac91faf773442d81c7e9c018d972036c81aae
craigbojan <craigbradley_1@live.co.uk>
2026-07-30 18:46:12 +0100

    Add files via upload

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -172,11 +172,9 @@
 #define VAR_TOHJO_FALLS_GIOVANNI_STATE                   0x4098
 #define VAR_LILYCOVE_CONTEST_LOBBY_STATE                 0x4099 //Not used in HnS but may be later
 #define VAR_ROUTE22_GIOVANNI_STATE                       0x409A
-#define VAR_ROUTE27_BAOBA_CALL                           0x409B 
-#define VAR_UNUSED_HNS_VAR4                              0x409C
-#define VAR_UNUSED_HNS_VAR5                              0x409D
-#define VAR_CHEAT_ENCOUNTER_ENABLED                      VAR_UNUSED_HNS_VAR4
-#define VAR_CHEAT_ENCOUNTER_SPECIES                      VAR_UNUSED_HNS_VAR5
+#define VAR_ROUTE27_BAOBA_CALL                              0x409B 
+#define VAR_CHEAT_ENCOUNTER_ENABLED                      0x409C
+#define VAR_CHEAT_ENCOUNTER_SPECIES                      0x409D
 #define VAR_UNUSED_HNS_VAR6                              0x409E
 #define VAR_UNUSED_HNS_VAR7                              0x409F// HnS Vars end
 #define VAR_SLATEPORT_HARBOR_STATE                       0x40A0
